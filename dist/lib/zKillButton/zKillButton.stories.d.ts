2bfd6a47969e64c23f581b2bc22ca59fa590fd4e
Aaron Kable <aaronkable@gmail.com>
2022-09-11 07:48:09 +0800

    update actions

---
+++ dist/lib/zKillButton/zKillButton.stories.d.ts
@@ -0,0 +1,6 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { default as ZKillButton } from './zKillButton';
+declare const _default: ComponentMeta<typeof ZKillButton>;
+export default _default;
+export declare const Primary: ComponentStory<typeof ZKillButton>;
+//# sourceMappingURL=zKillButton.stories.d.ts.map
